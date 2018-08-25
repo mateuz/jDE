115553d422606881459e286d72a286b81e8ff16a
Mateus Boiani <mateuz.boiani@gmail.com>
2018-08-24 23:11:11 -0300

    New features, getting time of execution, printing the parameters of executions, support multiple runs and fix the calc of number of functions evaluations

--- source/main.cpp
+++ source/main.cpp
@@ -17,10 +17,47 @@
 #include "F3.h"
 #include "F4.h"
 
+double stime(){
+  struct timeval tv;
+  struct timezone tz;
+  gettimeofday(&tv, &tz);
+  double mlsec = 1000.0 * ((double)tv.tv_sec + (double)tv.tv_usec/1000000.0);
+  return mlsec/1000.0;
+}
+
+void show_params(
+	uint n_runs,
+	uint NP,
+	uint n_evals,
+	uint n_dim,
+	std::string FuncObj
+){
+	printf(" | Number of Executions:                    %d\n", n_runs);
+	printf(" | Population Size:                         %d\n", NP);
+	printf(" | Number of Dimensions:                    %d\n", n_dim);
+	printf(" | Number of Function Evaluations:          %d\n", n_evals);
+	printf(" | Optimization Function:                   %s\n", FuncObj.c_str());
+}
+
+std::string toString(uint id){
+  switch( id ){
+    case 1:
+      return "Shifted Sphere";
+    case 2:
+      return "Shifted Rosenbrock";
+    case 3:
+      return "Shifted Griewank";
+    case 4:
+      return "Shifted Rastringin";
+    default:
+      return "Unknown";
+  }
+}
+
 int main(int argc, char * argv[]){
   uint NP = 50;
   uint ndim = 100;
-  uint nEvals = 10e4 * ndim;
+  uint n_evals = 10000 * ndim;
   vDouble gen(NP * ndim);
   vDouble n_gen(NP * ndim);
   vDouble fitness(NP, 0.0);
@@ -30,51 +67,49 @@ int main(int argc, char * argv[]){
   rng.seed(std::random_device{}());
 
   Benchmarks * B = new F2(ndim);
-  /*for( uint go = 1; go <= 4; go++ ){
-    switch (go) {
-      case 1:
-        B = new F1(ndim);
-        break;
-      case 2:
-        B = new F2(ndim);
-        break;
-      case 3:
-        B = new F3(ndim);
-        break;
-      case 4:
-        B = new F4(ndim);
-        break;
-    }
-  */
+
+  uint n_runs = 10;
+  uint f_id = 2;
+
+  printf(" +==============================================================+ \n");
+	printf(" |                      EXECUTION PARAMETERS                    | \n");
+	printf(" +==============================================================+ \n");
+	show_params(n_runs, NP, n_evals, ndim, toString(f_id));
+	printf(" +==============================================================+ \n");
 
   std::uniform_real_distribution<double> random(B->getMin(), B->getMax());
 
+  double tini, tend;
+  for( int go = 1; go <= n_runs; go++ ){
+    tini = stime();
     //randomly init genes
-  for( auto it = gen.begin(); it != gen.end(); it++ )
-    *it = random(rng);
+    for( auto it = gen.begin(); it != gen.end(); it++ )
+      *it = random(rng);
 
-  jDE * jde = new jDE(NP);
-
-  for( uint i = 0; i < NP; i++ )
-    fitness[i] = B->compute(gen, i * ndim);
-
-  for( uint run = 0; run < nEvals; run += NP){
-    jde->runDE(ndim, NP, gen, n_gen, B->getMin(), B->getMax());
+    jDE * jde = new jDE(NP);
 
     for( uint i = 0; i < NP; i++ )
-      n_fitness[i] = B->compute(n_gen, i * ndim);
+      fitness[i] = B->compute(gen, i * ndim);
 
-    jde->selection(ndim, NP, gen, n_gen, fitness, n_fitness);
-    jde->update();
-  }
-  double a = fitness[0];
-  uint pb = 0;
-  for( uint i = 0; i < NP; i++ ){
-    if( fitness[i] < a){
-      pb = i;
-      a = fitness[i];
+    for( uint run = 0; run < n_evals; run += NP){
+      jde->runDE(ndim, NP, gen, n_gen, B->getMin(), B->getMax());
+
+      for( uint i = 0; i < NP; i++ )
+        n_fitness[i] = B->compute(n_gen, i * ndim);
+
+      jde->selection(ndim, NP, gen, n_gen, fitness, n_fitness);
+      jde->update();
     }
+    double a = fitness[0];
+    uint pb = 0;
+    for( uint i = 0; i < NP; i++ ){
+      if( fitness[i] < a){
+        pb = i;
+        a = fitness[i];
+      }
+    }
+    tend = stime();
+    printf(" | Execution: %-2d Overall Best: %+.8lf Time(ms): %.8f\n", go, fitness[pb], tend-tini);
   }
-  printf("F :: %d best individual %-3i with %-5.20E.\n", B->getID(), pb, fitness[pb]);
   return 0;
 }
